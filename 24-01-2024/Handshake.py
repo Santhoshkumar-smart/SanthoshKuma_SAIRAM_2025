9e2c905dd9cc26474ff797f5a22e5729f1c7988c
Santhoshkumar-smart <157287623+Santhoshkumar-smart@users.noreply.github.com>
2024-01-26 20:57:02 +0530

    Create Handshake.py

---
+++ 24-01-2024/Handshake.py
@@ -0,0 +1,16 @@
+def handshake(n):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = handshake(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
