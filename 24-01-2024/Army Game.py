887605f23890db3847f28225fcceccd20e5ef572
Santhoshkumar-smart <157287623+Santhoshkumar-smart@users.noreply.github.com>
2024-01-26 20:58:39 +0530

    Create Army Game.py

---
+++ 24-01-2024/Army Game.py
@@ -0,0 +1,17 @@
+def gameWithCells(n, m):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = gameWithCells(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
